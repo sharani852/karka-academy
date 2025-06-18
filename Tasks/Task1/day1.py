fe5f205a176b861a162a8955729454550336dd28
sharani852 <sharani8523@gmail.com>
2025-06-18 17:28:49 +0530

    Add files via upload

---
+++ Tasks/Task1/day1.py
@@ -0,0 +1,76 @@
+#1
+print("Hello,World!")
+#2
+Name="NL Sharani"
+print("My name is",Name)
+#3
+Name="NL Sharani"
+Age=21
+print("My name is",Name)
+print("My age is",Age)
+#4
+Name="NL Sharani"
+Age=21
+Height=5.2
+is_car=True
+print("Name is",Name)
+print("Age is",Age)
+print("Height is",Height)
+print("is this a car",is_car)
+#5
+Name=input("Enter your name:")
+Age=input("Enter your age:")
+print("MY name is" + Name +"My age is"+Age)
+#6
+Name="NL Sharani"
+Age=21
+print(Name)
+print(Age)
+#7
+a=10
+b=20
+c=a+b
+print("The sum is",c)
+a=20
+b=10
+c=a-b
+print("The answer is",c)
+a=2
+b=5
+c=a*b
+print("The answer is",c)
+a=4
+b=2
+c=a/b
+print("The answer is",c)
+8
+for i in range(1,5):
+   print("*" * i)
+#9
+Name="Lulu"
+Age=21
+isStudent=True
+print("Name is",Name)
+print("Age is",Age)
+print("Is this a student",isStudent)
+Name="Guru"
+Age=Age+1
+isStudent=not isStudent
+print("Updated name",Name)
+print("Updated age",Age)
+print("Is this a student now",isStudent)
+#10
+name="NL Sharani"
+age=22
+dob="23.7.2004"
+height=5.3
+weight=40.7
+degree="BE"
+gender="Female"
+print("Name:",name)
+print("Age:",age)
+print("Date of birth:",dob)
+print("Height:",height)
+print("Weight:",weight)
+print("Degree:",degree)
+print("Gender:",gender)
